0b2a8552e8e6926d37c893e4992454783418cd7c
Dan Michaelson <dan@linkedbyair.net>
2014-03-04 15:08:30 -0500

    Persistence lab

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ var path = require('path');
 var socketio = require('socket.io');
 var express = require('express');
 
+var MongoClient = require('mongodb').MongoClient;
+
 // Configure the Express web server
 var router = express();
 var server = http.createServer(router);
@@ -17,37 +19,59 @@ var io = socketio.listen(server);
 // Global variables
 var sockets = [];
 
-// Whenever a new client connects
-io.on('connection', function(socket) {
-
-  // Add this client to our list of clients
-  sockets.push(socket);
-   
-  // Listen for when this client disconnects
-  socket.on('disconnect', function() {
-    // Remove this client from our list of clients
-    sockets.splice(sockets.indexOf(socket), 1);
-  });
-
-  // Listen for when this client sends a message
-  socket.on('message', function(message) {
+MongoClient.connect('mongodb://networks:yalent@ds033069.mongolab.com:33069/networks', function(err, db) {
+    if (err) throw err;
     
-    // Build a message from the input (in case we want to transform it)
-    var newMessage = {
-      name: message.name,
-      text: message.text,
-      color: message.color,
-      time: new Date()
-    };
+    // Get a reference to the messages collection in the database
+    var messages = db.collection('messages');
+    
+    // Whenever a new client connects
+    io.on('connection', function(socket) {
+    
+      // Add this client to our list of clients
+      sockets.push(socket);
 
-    // Broadcast this message to all clients
-    sockets.forEach(function(socket) {
-      socket.emit('message', newMessage);
+      // Listen for when this client disconnects
+      socket.on('disconnect', function() {
+        // Remove this client from our list of clients
+        sockets.splice(sockets.indexOf(socket), 1);
+      });
+      
+      // Send this client a list of all messages so far, by querying the database
+      // and going through each result.
+      messages.find().sort({time: 1}).each(function(err, message) {
+         if (err) throw err;
+         
+         socket.emit('message', message);
+         
+      });
+    
+      // Listen for when this client sends a message
+      socket.on('message', function(message) {
+        
+        // Build a message from the input (in case we want to transform it)
+        var newMessage = {
+          name: message.name,
+          text: message.text,
+          color: message.color,
+          time: new Date(),
+        };
+    
+        // Broadcast this message to all clients
+        sockets.forEach(function(socket) {
+          socket.emit('message', newMessage);
+        });
+        
+        // Store this message in the database (in the messages collection)
+        messages.insert(newMessage, function(err, inserted) {
+          if (err) throw err; 
+        });
+    
+      });
+    
     });
-
-  });
+    
+    // Start our web server
+    server.listen(process.env.PORT);
 
 });
-
-// Start our web server
-server.listen(process.env.PORT);
