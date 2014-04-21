7b91a824165f7f081f1574e97de75cec11b9da2d
yotam2 <yotam.hadar@yale.edu>
2014-04-21 16:31:01 -0400

    gimg2 fix

--- server.js
+++ server.js
@@ -114,7 +114,7 @@ MongoClient.connect('mongodb://yotam:monodb444@ds033069.mongolab.com:33069/yotam
 		                        console.log('-------------------');
 		                        console.log(urlStringz2[0]);
 		                        // Render the view
-		                        res.render('list', {list: list, total_votes: total_votes, urlString:urlStringz1[0],urlString:urlStringz2[0] //the url for the last submissiion img });
+		                        res.render('list', {list: list, total_votes: total_votes, urlStringz1:urlStringz1[0],urlStringz2:urlStringz2[0] //the url for the last submissiion img });
 		                    }
 		                });
 
@@ -157,6 +157,7 @@ MongoClient.connect('mongodb://yotam:monodb444@ds033069.mongolab.com:33069/yotam
                 function(error, response, body) {
             
                     if (!error && response.statusCode == 200) {
+                    	//getting the image for the top result
                         console.log('-----------------------------------');
                         console.log(list[0].name);
                         console.log('-----------------------------------');
@@ -167,15 +168,43 @@ MongoClient.connect('mongodb://yotam:monodb444@ds033069.mongolab.com:33069/yotam
                         imgResults.forEach(function(result) {
                             if (result.url) {
                                 console.log(result.url);
-                                urlStringz.push(result.url);
+                                urlStringz1.push(result.url);
                             }
                         });
             
-                        console.log(urlStringz);
+                        console.log(urlStringz1);
                         console.log('-------------------');
-                        console.log(urlStringz[1]);
-                        // Render the view
-                        res.render('list', {list: list, total_votes: total_votes, urlString:urlStringz[1] });
+                        console.log(urlStringz1[0]);
+
+		                request('https://ajax.googleapis.com/ajax/services/search/images?v=1.0&imgc=mono&q=' + name,
+		                
+		                function(error, response, body) {
+                    	//getting the image for the last submission
+		            
+		                    if (!error && response.statusCode == 200) {
+		                        console.log('-----------------------------------');
+		                        console.log(name);
+		                        console.log('-----------------------------------');
+		                        console.log(body);
+		                        var imgResults = JSON.parse(body).responseData.results;
+		                        console.log(imgResults);
+		                            
+		                        imgResults.forEach(function(result) {
+		                            if (result.url) {
+		                                console.log(result.url);
+		                                urlStringz2.push(result.url);
+		                            }
+		                        });
+		            
+		                        console.log(urlStringz2);
+		                        console.log('-------------------');
+		                        console.log(urlStringz2[0]);
+		                        // Render the view
+		                        res.render('list', {list: list, total_votes: total_votes, urlStringz1:urlStringz1[0],urlStringz2:urlStringz2[0] //the url for the last submissiion img });
+		                    }
+		                });
+
+
                     }
                 });
                                 
