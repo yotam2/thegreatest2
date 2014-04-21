dc1235f1f00ac4ccbd76435a88dc3a1966b1bde0
yotam2 <yotam.hadar@yale.edu>
2014-04-21 17:33:12 -0400

    background fix

--- server.js
+++ server.js
@@ -176,31 +176,9 @@ MongoClient.connect('mongodb://yotam:monodb444@ds033069.mongolab.com:33069/yotam
                         console.log('-------------------');
                         console.log(urlStringz1[0]);
 
-                        request('https://ajax.googleapis.com/ajax/services/search/images?v=1.0&imgc=mono&rsz=2&q=' + name,
-                        
-                        function(error, response, body) {
-                        //getting the image for the last submission
-                    
-                            if (!error && response.statusCode == 200) {
-                                console.log('-----------------------------------');
-                                console.log(name);
-                                console.log('-----------------------------------');
-                                console.log(body);
-                                var imgResults = JSON.parse(body).responseData.results;
-                                console.log(imgResults);
-                                    
-                                imgResults.forEach(function(result) {
-                                    if (result.url) {
-                                        console.log(result.url);
-                                        urlStringz2.push(result.url);
-                                    }
-                                });
-                    
-                                console.log(urlStringz2);
-                                console.log('-------------------');
-                                console.log(urlStringz2[0]);
+
                                 // Render the view
-                                res.render('list', {list: list, total_votes: total_votes, urlString1:urlStringz1[0],urlString2:urlStringz2[0] });//the url for the last submissiion img });
+                                res.render('list', {list: list, total_votes: total_votes, urlString1:urlStringz1[0] });//the url for the last submissiion img });
                             }
                         });
 
